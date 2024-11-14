af15b00c59e8bbc87e8fb56fe48bae855316c49b
MohamadAbedAlkader <mam10102004@gmail.com>
2024-11-14 14:40:38 +0200

    Beta

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,34 @@
+import javax.print.DocFlavor;
+
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+            String str = args[0];
+            int count = 0;
+            int count2 = 0;
+            boolean flag = true;
+            String a = "AEFHILMNORSX";
+            while(str.length()!=count){
+                while(a.length()!=count2){
+                    if(str.charAt(count) == a.charAt(count2)) {
+                        flag = false;
+                        break;
+                    }
+                    count2++;
+                }
+                if(!flag)
+                System.out.println("Give me an " +  str.charAt(count) + ": "+str.charAt(count)+"!" );
+
+                else
+                    System.out.println("Give me a " +  str.charAt(count) + ": "+str.charAt(count)+"!" );
+
+                count++;
+            }
+            System.out.println("What does that spell?");
+
+            for(int i =0; i < 3; i++)
+            System.out.println(str + "!!!");
+
+
         }
 }
