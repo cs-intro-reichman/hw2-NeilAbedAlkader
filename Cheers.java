f33fc44e3bc3c11d6022254ac8896cbe45bcdd14
MohamadAbedAlkader <mam10102004@gmail.com>
2024-11-14 14:45:18 +0200

    A

--- Cheers.java
+++ Cheers.java
@@ -17,10 +17,10 @@ public class Cheers {
                     count2++;
                 }
                 if(!flag)
-                System.out.println("Give me an " +  str.charAt(count) + ": "+str.charAt(count)+"!" );
+                System.out.println("Give me a " +  str.charAt(count) + ": "+str.charAt(count)+"!" );
 
                 else
-                    System.out.println("Give me a " +  str.charAt(count) + ": "+str.charAt(count)+"!" );
+                    System.out.println("Give me an " +  str.charAt(count) + ": "+str.charAt(count)+"!" );
 
                 count++;
             }
