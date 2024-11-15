d0984aed3bdcd89afe8c21b98751969c37dfc3df
MohamadAbedAlkader <mam10102004@gmail.com>
2024-11-15 19:26:11 +0200

    Alpha

--- Cheers.java
+++ Cheers.java
@@ -2,9 +2,8 @@ import javax.print.DocFlavor;
 
 public class Cheers {
     public static void main(String[] args) {
-        // Parse inputs
-        String str = args[0].toUpperCase(); // Convert input word to uppercase
-        int var1 = Integer.parseInt(args[1]); // Number of times to cheer
+        String str = args[0].toUpperCase();
+        int var1 = Integer.parseInt(args[1]);
         String specialLetters = "AEFHILMNORSX";
 
         for (int i = 0; i < str.length(); i++) {
